0b1b072abff7358095aec1db22f753644f4c7165
sneakerhax <sneakerhax@gmail.com>
2025-10-06 22:31:08 -0700

    Update dockerhub_update.sh to push both linux/amd64 and linux/arm64 images

--- dockerhub_update.sh
+++ dockerhub_update.sh
@@ -1,3 +1,3 @@
 # build and push PNT3 to Docker Hub
-docker buildx build --platform linux/amd64 -t sneakerhax/pnt3 -f ./Dockerfile . --push
+docker buildx build --platform linux/amd64,linux/arm64 -t sneakerhax/pnt3 -f ./Dockerfile . --push
 # docker push sneakerhax/pnt3:latest
